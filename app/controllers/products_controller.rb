a47cd44d4eae5ad27c4ecaae725432788bfaf46f
casoler <casoler11@gmail.com>
2016-07-12 20:51:42 -0500

    delete useless pages and add redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,8 @@ class ProductsController < ApplicationController
       flag_image: params[:flag_image]
     )
     @product.save
+
+    redirect_to "/products/#{@product.id}"    
   end
 
   def show
@@ -37,5 +39,14 @@ class ProductsController < ApplicationController
       country_of_origin: params[:country_of_origin], 
       flag_image: params[:flag_image]
     )
+
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+
+    redirect_to "/products"
   end
 end
